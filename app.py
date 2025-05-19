e5e9d861ada46082b1b9b2af15677aed3071ca01
Simran-bagga87 <167749495+Simran-bagga87@users.noreply.github.com>
2025-05-19 15:27:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,28 +3,12 @@ import pandas as pd
 from openpyxl import load_workbook
 import numpy as np
 import gspread
-from google.oauth2.service_account import Credentials
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
 import json
-service_account_info = {
-    "type": st.secrets["gcp_service_account"]["type"],
-    "project_id": st.secrets["gcp_service_account"]["project_id"],
-    "private_key_id": st.secrets["gcp_service_account"]["private_key_id"],
-    "private_key": st.secrets["gcp_service_account"]["private_key"],
-    "client_email": st.secrets["gcp_service_account"]["client_email"],
-    "client_id": st.secrets["gcp_service_account"]["client_id"],
-    "auth_uri": st.secrets["gcp_service_account"]["auth_uri"],
-    "token_uri": st.secrets["gcp_service_account"]["token_uri"],
-    "auth_provider_x509_cert_url": st.secrets["gcp_service_account"]["auth_provider_x509_cert_url"],
-    "client_x509_cert_url": st.secrets["gcp_service_account"]["client_x509_cert_url"],
-    "universe_domain": st.secrets["gcp_service_account"]["universe_domain"]
-}
 
-credentials = Credentials.from_service_account_info(service_account_info)
+gc = gspread.Client(None) 
+sh = gc.open_by_url('https://docs.google.com/spreadsheets/d/1EsAikSPKVwGTlnlScVt6zquRMGiYguNTAvxnJWnAvpI/edit?pli=1&gid=0#gid=0')
 
-gc = gspread.authorize(credentials)
-
-sh = gc.open('Inventory') 
 worksheet = sh.worksheet('Sheet1')
 data = get_as_dataframe(worksheet)
 
