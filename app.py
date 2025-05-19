6e5752d0d2c2b25b17b00c6bf00ee8c6088d1c76
Simran-bagga87 <167749495+Simran-bagga87@users.noreply.github.com>
2025-05-19 12:36:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,25 @@ import numpy as np
 import gspread
 from google.oauth2.service_account import Credentials
 from gspread_dataframe import get_as_dataframe, set_with_dataframe
-
 import json
+service_account_info = {
+    "type": st.secrets["type"],
+    "project_id": st.secrets["project_id"],
+    "private_key_id": st.secrets["private_key_id"],
+    "private_key": st.secrets["private_key"],
+    "client_email": st.secrets["client_email"],
+    "client_id": st.secrets["client_id"],
+    "auth_uri": st.secrets["auth_uri"],
+    "token_uri": st.secrets["token_uri"],
+    "auth_provider_x509_cert_url": st.secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": st.secrets["client_x509_cert_url"],
+    "universe_domain": st.secrets["universe_domain"]
+}
 
-json_path = "sunlit-sweep-434508-s0-911433e0ac36.json"
-gc = gspread.service_account(filename=json_path)
+credentials = Credentials.from_service_account_info(service_account_info)
+
+gc = gspread.authorize(credentials)
+
 sh = gc.open('Inventory') 
 worksheet = sh.worksheet('Sheet1')
 data = get_as_dataframe(worksheet)
